feb1313e4d5cbc0d2046fc374be1ce5dfb60d1ad
nolik <nolik03@gmail.com>
2017-07-23 04:08:46 +0300

    add advert repository

--- src/main/java/com/getItFree/model/Advert.java
+++ src/main/java/com/getItFree/model/Advert.java
@@ -1,23 +1,23 @@
 package com.getItFree.model;
 
-import org.springframework.data.annotation.Id;
-import lombok.AccessLevel;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  * Created by nolik on 03.07.17.
  */
 @Data
+@AllArgsConstructor
+@Document
 public class Advert {
     @Id
     private int id;
-    private byte[] image;
     private String head;
     private String content;
-    private Timestamp date;
-
+    private Date date;
+    private String imageUrl;
 }
