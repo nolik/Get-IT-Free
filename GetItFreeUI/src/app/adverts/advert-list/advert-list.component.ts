509ebec3b999bad8a548bc4d90f8ef7b0e185381
nolik <nolik03@gmail.com>
2017-11-13 00:18:49 +0300

    Restructure advert components.

--- GetItFreeUI/src/app/adverts/advert-list/advert-list.component.ts
+++ GetItFreeUI/src/app/adverts/advert-list/advert-list.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {AdvertModel} from './advert.model';
+import {AdvertModel} from '../advert/advert.model';
 
 @Component({
-  selector: 'app-adverts',
-  templateUrl: './adverts.component.html',
-  styleUrls: ['./adverts.component.css']
+  selector: 'app-advert-list',
+  templateUrl: './advert-list.component.html',
+  styleUrls: ['./advert-list.component.css']
 })
-export class AdvertsComponent implements OnInit {
+export class AdvertListComponent implements OnInit {
   adverts: AdvertModel[] = [
     new AdvertModel(1, 'test header', 'test content', new Date, 'https://thumb9.shutterstock.com/display_pic_with_logo/3521228/391005625/' +
       'stock-photo-glasses-of-light-and-dark-beer-on-a-pub-background-391005625.jpg', false),
@@ -24,10 +24,6 @@ export class AdvertsComponent implements OnInit {
   }
 
   ngOnInit() {
-  }
-
-  orderAdvert(advert: AdvertModel) {
-    advert.ordered = true;
   }
 
 }
--- GetItFreeUI/src/app/app.module.ts
+++ GetItFreeUI/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AdvertsComponent } from './adverts/adverts.component';
+import { AdvertListComponent } from './adverts/advert-list/advert-list.component';
 import { AddAdvertComponent } from './adverts/add-advert/add-advert.component';
+import { AdvertComponent } from './adverts/advert/advert.component';
+import { AdvertDetailComponent } from './adverts/advert-detail/advert-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AdvertsComponent,
-    AddAdvertComponent
+    AdvertListComponent,
+    AddAdvertComponent,
+    AdvertComponent,
+    AdvertDetailComponent
   ],
   imports: [
     BrowserModule
