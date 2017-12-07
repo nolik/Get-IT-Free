36f0246b7c914a661437754ea6ce1658732d979b
nolik <nolik03@gmail.com>
2017-12-07 23:57:56 +0300

    Add 404 page.

--- GetItFreeUI/src/app/app.module.ts
+++ GetItFreeUI/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { AdvertDetailComponent } from './adverts/advert-detail/advert-detail.com
 import {DropdownDirective} from './shared/dropdown.directive';
 import {AdvertService} from './adverts/advert.service';
 import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: AdvertListComponent },
   { path: 'advert-list', component: AdvertListComponent },
   { path: 'add-advert', component: AddAdvertComponent },
-  { path: 'advert-detail/:id', component: AdvertDetailComponent }
+  { path: 'advert-detail/:id', component: AdvertDetailComponent },
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -26,7 +28,8 @@ const appRoutes: Routes = [
     AddAdvertComponent,
     AdvertComponent,
     AdvertDetailComponent,
-    DropdownDirective
+    DropdownDirective,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
