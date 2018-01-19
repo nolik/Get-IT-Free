78ab90eb9cb0e6791c4bf14cf379d9d90cd12b19
nolik <nolik03@gmail.com>
2018-01-20 00:01:27 +0300

    Add AppConfig for SpringBoot.

---
+++ GetItFreeService/src/main/java/com/getItFree/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.getItFree;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("PUT",
+                                "GET",
+                                "POST");
+    }
+}
