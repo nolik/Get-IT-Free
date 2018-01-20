045de6ef93a5e12745db4920e86879b18024658b
nolik <nolik03@gmail.com>
2018-01-20 16:42:14 +0300

    Devide Advert Service with interface level.

---
+++ GetItFreeService/src/main/java/com/getItFree/service/AdvertServiceImpl.java
@@ -0,0 +1,48 @@
+package com.getItFree.service;
+
+import com.getItFree.model.Advert;
+import com.getItFree.repository.AdvertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AdvertServiceImpl implements AdvertService {
+
+    private final AdvertRepository advertRepository;
+
+    //temporary initialization of db
+    private static List<Advert> adverts = new ArrayList<>();
+
+//    static {
+//        adverts.add(new Advert(1, "пивко", "забирай добрый люд пивко", Calendar.getInstance().getTime(),
+//                "https://thumb9.shutterstock.com/display_pic_with_logo/3521228/391005625/stock-photo-glasses-of-light-and-dark-beer-on-a-pub-background-391005625.jpg", false));
+//        adverts.add(new Advert(2, "пивко2", "забирай добрый люд пивко", Calendar.getInstance().getTime(),
+//                "https://thumb9.shutterstock.com/display_pic_with_logo/3521228/391005625/stock-photo-glasses-of-light-and-dark-beer-on-a-pub-background-391005625.jpg", false));
+//        adverts.add(new Advert(3, "пивко3", "забирай добрый люд пивко", Calendar.getInstance().getTime(),
+//                "https://thumb9.shutterstock.com/display_pic_with_logo/3521228/391005625/stock-photo-glasses-of-light-and-dark-beer-on-a-pub-background-391005625.jpg", false));
+//    }
+
+    @Autowired
+    public AdvertServiceImpl(AdvertRepository advertRepository) {
+        this.advertRepository = advertRepository;
+    }
+
+    @PostConstruct
+    public void init(){
+        advertRepository.save(adverts);
+    }
+
+
+    public List<Advert> findAllAdvertsFromDB(){
+        List<Advert> repositoryAll=  advertRepository.findAll();
+        return repositoryAll;
+    }
+
+    public Advert addNewAdvert(Advert advert){
+        return advertRepository.save(advert);
+    }
+}
