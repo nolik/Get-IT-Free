cf3324a2d9e54c094854bd140ffaf3f10a1638e6
nolik <nolik03@gmail.com>
2018-01-23 01:35:08 +0300

    Update Advert Mapping with _id BigInteger type.

--- GetItFreeService/src/main/java/com/getItFree/model/Advert.java
+++ GetItFreeService/src/main/java/com/getItFree/model/Advert.java
@@ -2,10 +2,12 @@ package com.getItFree.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.bson.types.ObjectId;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigInteger;
 import java.util.Date;
 
 /**
@@ -13,10 +15,12 @@ import java.util.Date;
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Document
 public class Advert {
     @Id
-    private ObjectId id;
+    @Generated
+    private BigInteger _id;
     private String head;
     private String content;
     private Date date;
@@ -24,7 +28,6 @@ public class Advert {
     private boolean ordered;
 
     public Advert(String head, String content, Date date, String imageUrl, boolean ordered) {
-        this.id = new ObjectId();
         this.head = head;
         this.content = content;
         this.date = date;
