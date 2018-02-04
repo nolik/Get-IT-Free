ff7d866c5db5b91a4631c967fd1997374b5fff13
nolik <nolik03@gmail.com>
2018-02-05 01:04:57 +0300

    migrate to Gradle.

--- src/main/java/com/getItFree/model/AdvertDTO.java
+++
@@ -1,7 +0,0 @@
-package com.getItFree.model;
-
-/**
- * Created by nolik on 03.07.17.
- */
-public class AdvertDTO {
-}
--- src/main/java/com/getItFree/repository/AdvertRepository.java
+++
@@ -1,9 +0,0 @@
-package com.getItFree.repository;
-
-import com.getItFree.model.Advert;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdvertRepository extends MongoRepository<Advert, String>{
-}
