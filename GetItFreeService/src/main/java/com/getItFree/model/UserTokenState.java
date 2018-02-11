e555967204b2ea4bd2b594a6f0cca4334fdd4d5e
nolik <nolik03@gmail.com>
2018-02-11 21:10:46 +0300

    Add authentication part.

--- GetItFreeService/src/main/java/com/getItFree/model/UserTokenState.java
+++ GetItFreeService/src/main/java/com/getItFree/model/UserTokenState.java
@@ -1,9 +1,16 @@
 package com.getItFree.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class UserTokenState {
+    private String access_token;
+    private Long expires_in;
+
+    public UserTokenState(String access_token, long expires_in) {
+        this.access_token = access_token;
+        this.expires_in = expires_in;
+    }
 }
