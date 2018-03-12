e4f671c1e5d0864f1e52a58d1f6c8cadea645ebe
nolik <nolik03@gmail.com>
2018-03-12 21:10:50 +0300

    UI part - add api.service .

--- GetItFreeUI/src/app/service/config.service.ts
+++ GetItFreeUI/src/app/service/config.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {environment} from '../../environments/environment';
 
 @Injectable()
 export class ConfigService {
@@ -18,17 +17,11 @@ export class ConfigService {
 
   private _change_password_url = this._api_url + '/changePassword';
 
-  private _reset_credentials_url = this._user_url + '/reset-credentials';
-
   private _signup_url = this._api_url + '/signup';
 
   private _adverts_url = this._api_url + '/adverts';
 
-  private _add_advert_url= this._api_url + 'add-advert';
-
-  get reset_credentials_url(): string {
-    return this._reset_credentials_url;
-  }
+  private _add_advert_url = this._api_url + 'add-advert';
 
   get refresh_token_url(): string {
     return this._refresh_token_url;
